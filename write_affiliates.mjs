df1a9197a8de7e72c5aad6bab9df9e1f01fa7eee
Mr-Ess <kemetrise@gmail.com>
2026-06-02 16:51:40 +0300

    chore: add utility scripts (_tmp_write.js, write_affiliates.mjs)

---
+++ write_affiliates.mjs
@@ -0,0 +1,7 @@
+﻿import { readFileSync, writeFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const target = join(__dirname, "src", "pages", "Affiliates.tsx");
+writeFileSync(target, readFileSync(join(__dirname, "affiliates_new.tsx"), "utf8"), "utf8");
+console.log("done");
